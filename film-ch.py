d12a4df4c822d43daa54555616457b28ede244b1
nedius <superdenisid@gmail.com>
2025-05-18 00:25:29 +0300

    Refactor film checker to enhance GraphQL integration and add search functionality

--- film-ch.py
+++ film-ch.py
@@ -1,37 +1,32 @@
+from typing import List
+import urllib.parse as urlparse
 import typer
+from typing_extensions import Annotated
+import requests
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
-app = typer.Typer(name="Film Checker", help="A simple way to check films and their details.", add_completion=False, pretty_exceptions_enable=False)
+app = typer.Typer(
+    name="Film Checker", 
+    help="A simple way to check films and their details.", 
+    add_completion=False, 
+    pretty_exceptions_enable=False
+)
 
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-    
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+gqlApiUrl = "https://graph.imdbapi.dev/v1"
+def searchApiUrl(query):
+    query = urlparse.quote_plus(query)
+    return f"https://v3.sg.media-imdb.com/suggestion/x/{query}.json"
 
-@app.command()
-def gqldemo():
-    """
-    A simple GraphQL demo using the gql library.
-    """
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url=gqlApiUrl, ssl=True)
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport, fetch_schema_from_transport=True)
 
-    # Select your transport with a defined url endpoint
-    transport = AIOHTTPTransport(url="https://graph.imdbapi.dev/v1", ssl=True)
-
-    # Create a GraphQL client using the defined transport
-    client = Client(transport=transport, fetch_schema_from_transport=True)
-
-    # Provide a GraphQL query
-    query = gql(
-        """
-        query titleById {
-        title(id: "tt0944947") {
+# GraphQL queries
+titleById = """
+    query titleById($id: ID!) {
+        title(id: $id) {
             id
             type
             is_adult
@@ -105,17 +100,129 @@ def gqldemo():
             characters
             }
         }
+    }
+"""
+
+personById = """
+    query nameById($id: ID!) {
+        name(id: $id) {
+            id
+            display_name
+            avatars {
+                url
+                width
+                height
+            }
+            alternate_names
+            birth_year
+            birth_location
+            death_year
+            death_location
+            dead_reason
+            known_for {
+                id
+                primary_title
+            }
         }
-    """
-    )
+    }
+"""
+
+def searchByName(name):
+    response = requests.get(searchApiUrl(name))
+    if response.status_code != 200:
+        raise Exception(f"Error: {response.status_code}")
+    data = response.json()
+    
+    for item in data["d"]:
+        print("id: ", item["id"])
+        print("name: ", item["l"])
+        print("summary: ", item["s"])
+        print("---------------------")
+
+# @app.command()
+def getTitle(id):
+    # Provide a GraphQL query
+    query = gql(titleById)
 
     # Execute the query on the transport
-    result = client.execute(query)
-    print(result)
+    result = client.execute(query, variable_values={"id": "tt0944947"})
+    # Print the result
+    printTitleResult(result)
+    
+def printTitleResult(result):
+    # Print the result
+    # print(result)
+    
+    # Print the title
+    print("Title: ", result["title"]["primary_title"])
+    # Print the plot
+    print("Plot: ", result["title"]["plot"])
+    # Print the rating
+    votes = result["title"]["rating"]["votes_count"]
+    print("Rating: ", result["title"]["rating"]["aggregate_rating"], f"({votes} votes)")
+    # Print the genres
+    print("Genres: ", ", ".join(result["title"]["genres"]))
+    # Print the certificates
+    for certificate in result["title"]["certificates"]:
+        print("Certificate: ", certificate["country"]["name"], certificate["rating"])
+    # Print the spoken languages
+    for language in result["title"]["spoken_languages"]:
+        print("Language: ", language["name"])
+    # Print the origin countries
+    for country in result["title"]["origin_countries"]:
+        print("Country: ", country["name"])
+    # Print the critic review
+    if result["title"]["critic_review"] is not None:
+        # Print the critic review
+        print("Critic Review: ", result["title"]["critic_review"]["score"], result["title"]["critic_review"]["review_count"])
+    
+# @app.command()
+def getPerson(id):
+    # Provide a GraphQL query
+    query = gql(personById)
+
+    # Execute the query on the transport
+    result = client.execute(query, variable_values={"id": "nm0908094"})
+    # Print the result
+    printPersonResult(result)
+    
+def printPersonResult(result):
+    # Print the result
+    # print(result)
+    
+    # Print the name
+    print("Name: ", result["name"]["display_name"])
+    # Print the birth year
+    print("Birth Year: ", result["name"]["birth_year"])
+    # Print the birth location
+    print("Birth Location: ", result["name"]["birth_location"])
+    # Print the death year
+    print("Death Year: ", result["name"]["death_year"])
+    # Print the death location
+    print("Death Location: ", result["name"]["death_location"])
+    # Print the dead reason
+    print("Dead Reason: ", result["name"]["dead_reason"])
+    # Print the known for
+    for known in result["name"]["known_for"]:
+        print("Known For: ", known["primary_title"])
+    
+def main(name: List[str], person: Annotated[bool, typer.Option("--person", "-p")] = False, title: Annotated[bool, typer.Option("--title", "-t")] = True):
+    name = " ".join(name)
+    print(f"Searching: {name}")
+    
+    # if (person):
+    #     # Search for a person
+    #     print("Searching for a person...")
+    #     # searchPerson(name)
+    # elif (title):
+    #     # Search for a title
+    #     print("Searching for a title...")
+    #     getTitle(name)
+        
+    searchByName(name)
 
 if __name__ == "__main__":
-    app()
-    
+    typer.run(main)
     
 # Programm block sheme
 # query (person name or title name) -> find person or title id -> show details
